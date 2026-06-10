6621fd52111fc78cf7863a8087b98e926bfb838d
Param <param15.veer.singh@gmail.com>
2026-06-10 15:11:45 +0530

    chore: configure terraform for local floci emulator

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,26 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region                      = var.aws_region
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    apigatewayv2 = "http://localhost:4566"
+    cloudfront   = "http://localhost:4566"
+    cloudtrail   = "http://localhost:4566"
+    cloudwatch   = "http://localhost:4566"
+    dynamodb     = "http://localhost:4566"
+    iam          = "http://localhost:4566"
+    lambda       = "http://localhost:4566"
+    s3           = "http://localhost:4566"
+    sns          = "http://localhost:4566"
+    sts          = "http://localhost:4566"
+  }
 
   default_tags {
     tags = {
@@ -26,6 +45,7 @@ provider "aws" {
     }
   }
 }
+
 
 module "database" {
   source = "./modules/database"
